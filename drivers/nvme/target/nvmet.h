cccf383b20d2c19d2804b4d77820e24fe7a0bc25
Mohamed Khalfella <mkhalfella@purestorage.com>
2025-10-03 15:56:13 -0700

    nvmet: Rapid Path Failure Recovery set controller identify fields
    TP8028 Rapid Path Failure Recovery defined new fields in controller
    identify response. The newly defined fields are:
    
    - CIU (Controller Instance UNIQUIFIER): is an 8bit non-zero value that
    is assigned a random value when controller first created. The value is
    expected to be incremented when RDY bit in CSTS register is asserted
    - CIRN (Controller Instance Random Number): is 64bit random value that
    gets generated when controller is crated. CIRN is regenerated everytime
    RDY bit is CSTS register is asserted.
    - CCRL (Cross-Controller Reset Limit) is an 8bit value that defines the
    maximum number of in-progress controller reset operations. CCRL is
    hardcoded to 4 as recommended by TP8028.
    
    TP4129 KATO Corrections and Clarifications defined CQT (Command Quiesce
    Time) which is used along with KATO (Keep Alive Timeout) to set an upper
    time limit for attempting Cross-Controller Recovery. For NVME subsystem
    CQT is set to 0 by default to keep the current behavior. The value can
    be set from configfs if needed.
    
    Make the new fields available for IO controllers only since TP8028 is
    not very useful for discovery controllers.
    
    Signed-off-by: Mohamed Khalfella <mkhalfella@purestorage.com>

--- drivers/nvme/target/nvmet.h
+++ drivers/nvme/target/nvmet.h
@@ -264,7 +264,10 @@ struct nvmet_ctrl {
 
 	uuid_t			hostid;
 	u16			cntlid;
+	u16			cqt;
+	u8			ciu;
 	u32			kato;
+	u64			cirn;
 
 	struct nvmet_port	*port;
 
@@ -331,6 +334,7 @@ struct nvmet_subsys {
 #ifdef CONFIG_NVME_TARGET_DEBUGFS
 	struct dentry		*debugfs_dir;
 #endif
+	u16			cqt;
 	u16			max_qid;
 
 	u64			ver;
--- include/linux/nvme.h
+++ include/linux/nvme.h
@@ -21,6 +21,9 @@
 #define NVMF_TRADDR_SIZE	256
 #define NVMF_TSAS_SIZE		256
 
+#define NVMF_CQT_MS		0
+#define NVMF_CCR_LIMIT		4
+
 #define NVME_DISC_SUBSYS_NAME	"nqn.2014-08.org.nvmexpress.discovery"
 
 #define NVME_NSID_ALL		0xffffffff
@@ -328,7 +331,10 @@ struct nvme_id_ctrl {
 	__le16			crdt1;
 	__le16			crdt2;
 	__le16			crdt3;
-	__u8			rsvd134[122];
+	__u8			rsvd134[1];
+	__u8			ciu;
+	__le64			cirn;
+	__u8			rsvd144[112];
 	__le16			oacs;
 	__u8			acl;
 	__u8			aerl;
@@ -362,7 +368,9 @@ struct nvme_id_ctrl {
 	__u8			anacap;
 	__le32			anagrpmax;
 	__le32			nanagrpid;
-	__u8			rsvd352[160];
+	__u8			rsvd352[34];
+	__le16			cqt;
+	__u8			rsvd388[124];
 	__u8			sqes;
 	__u8			cqes;
 	__le16			maxcmd;
@@ -389,7 +397,8 @@ struct nvme_id_ctrl {
 	__u8			msdbd;
 	__u8			rsvd1804[2];
 	__u8			dctype;
-	__u8			rsvd1807[241];
+	__u8			ccrl;
+	__u8			rsvd1808[240];
 	struct nvme_id_power_state	psd[32];
 	__u8			vs[1024];
 };
