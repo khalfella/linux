bc92b808e0d95d4c7314638d9e3842dafa781188
Mohamed Khalfella <mkhalfella@purestorage.com>
2025-10-03 15:56:20 -0700

    nvme: Implement cross-controller reset recovery
    A host that has more than one path connecting to an nvme subsystem
    typically has an nvme controller associated with every path. This is
    mostly applicable to nvmeof. If one path goes down, inflight IOs on that
    path should not be retried immediately on another path because this
    could lead to data corruption as described in TP4129. TP8028 defines
    cross-controller reset mechanism that can be used by host to terminate
    IOs on the failed path using one of the remaining healthy paths. Only
    after IOs are terminated, or long enough time passes as defined by
    TP4129, inflight IOs should be retried on another path. Implement core
    cross-controller reset shared logic to be used by the transports.
    
    Signed-off-by: Mohamed Khalfella <mkhalfella@purestorage.com>

--- drivers/nvme/host/constants.c
+++ drivers/nvme/host/constants.c
@@ -46,6 +46,7 @@ static const char * const nvme_admin_ops[] = {
 	[nvme_admin_virtual_mgmt] = "Virtual Management",
 	[nvme_admin_nvme_mi_send] = "NVMe Send MI",
 	[nvme_admin_nvme_mi_recv] = "NVMe Receive MI",
+	[nvme_admin_cross_ctrl_reset] = "Cross Controller Reset",
 	[nvme_admin_dbbuf] = "Doorbell Buffer Config",
 	[nvme_admin_format_nvm] = "Format NVM",
 	[nvme_admin_security_send] = "Security Send",
